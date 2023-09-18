41eb64c8b61d042d272a7a39e90dedf30a4a0d22
naz <p24nthompson@fairfieldprep.org>
2023-09-18 12:21:38 -0400

    both lab turn in

--- lab_2-2.py
+++ lab_2-2.py
@@ -1,13 +1,15 @@
 # 1) Create a Python file named lab_2-2.py
 
 # 2) Set a equal to 3 and b equal to a
-
+a = 3
+b = a
 # 3) Print the value of b and the value of a.
-
+print ( b,a )
 # 4) What is the value of each variable and why? (Answer as a comment)
-
+#that it had poped up with two 3's spaced apart
 # 5) Set a equal to 7.
-
+a = 7
 # 6) Print the value of a and the value of b
-
+print (a,b)
 # 7) What is the value of each variable and why? (Answer as a comment)
+# a swiched to 7 as b had keep the same number it was they time before when it was ran
